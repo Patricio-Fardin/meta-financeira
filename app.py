f8a3820e235815232e800a4d50e0fb2426ffa268
Patricio Fardin <patricio_fardin@hotmail.com>
2023-11-06 19:26:25 -0300

    adicionando arquivo erro.html

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import math
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template('index.html')
 
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    if request.method == 'POST':
+        try:
+            objetivo = float(request.form['objetivo'])
+            poupanca_mensal = float(request.form['poupanca_mensal'])
+            
+            tempo = math.ceil(objetivo / poupanca_mensal)
+            
+            return render_template('resultado.html', tempo=tempo)
+        except Exception as e:
+            return render_template('erro.html', erro=str(e))
 
-@app.route("/resultado")
-def resultado():
-  return render_template("resultado.html")
+if __name__ == '__main__':
+    app.run(debug=True)
